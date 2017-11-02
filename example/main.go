2b72421c33a97982017ca4d5432aa6a930a78e54
tudyzhb <tudyzhb@gmail.com>
2017-11-02 18:01:59 +0800

    feat:example of sync table

--- example/main.go
+++ example/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/suboat/sorm"
+	"github.com/suboat/sorm/log"
 
 	// import drivers
 	_ "github.com/suboat/sorm/pg"
+
+	"time"
 )
 
 // Config
@@ -15,6 +18,31 @@ type Config struct {
 	PgUser string
 	PgPsw  string
 	PgDb   string
+}
+
+// inherit example
+type DemoPerson struct {
+	Uid        orm.Uid          `sorm:"size(36) unique"`
+	DemoAnimal `bson:",inline"` // mgo default embed fields
+
+	FirstName string    `sorm:"size(32) index"`
+	LastName  string    `sorm:"size(64) index"`
+	Age       int       `sorm:"index"`
+	Birthday  time.Time `sorm:"index"`
+	Message   string
+	Address   demoAddress
+	Password  []byte
+}
+
+type DemoAnimal struct {
+	Height float32 `sorm:"index"`
+	Weight float32
+	Length int64
+}
+
+type demoAddress struct {
+	Road   string
+	Number int
 }
 
 // example
@@ -28,6 +56,7 @@ func main() {
 			PgDb:   "example",
 		}
 		db  orm.Database
+		tb orm.Model
 		err error
 	)
 
@@ -36,7 +65,14 @@ func main() {
 
 	if db, err = orm.New("pg", connStr); err != nil {
 		panic(err)
+	} else {
+		log.Debug("connect success: ", db.String())
 	}
 
-	println("connect success: ", db.String())
+	// sync table and fields with struct
+	tb = db.Model("demoperson")
+	if err = tb.EnsureIndexWithTag(&DemoPerson{});err != nil {
+		panic(err)
+	}
+
 }
